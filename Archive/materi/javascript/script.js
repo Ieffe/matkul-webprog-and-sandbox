082bf60d59507e3178bb0f1d3f4c8cf6794decd9
Ieffe <novditha@gmail.com>
2020-04-03 13:16:07 +0800

    video kedua

--- Archive/materi/javascript/script.js
+++ Archive/materi/javascript/script.js
@@ -1,11 +1,37 @@
 (function () {
-    "use strict";
-    var myfunction = function (event) {
-        alert("Message goes here");
-        event.preventDefault();
-    };
+    "use strict"; //mencegah variabel liar yang tidak didefinisi
+    // var myfunction = function (event) {
+    //     alert("Message goes here");
+    //     event.preventDefault();
+    // };
     
-    var form = document.getElementById("cart-hplus");
-    form.addEventListener("submit", myfunction, true);
+    // var form = document.getElementById("cart-hplus");
+    // form.addEventListener("submit", myfunction, true);
+    // konten video 1
+
+    document.addEventListener("DOMContentLoaded", function(){
+        var state = document.getElementById("s-state");
+
+        document.getElementById("cart-hplus").addEventListener("submit", estimateTotal);
+
+        var btnEstimate = document.getElementById("btn-estimate");
+
+        btnEstimate.disabled = true;
+
+        state.addEventListener("change", function(){
+            if (state.value === ""){
+                btnEstimate.disabled = true;
+            } 
+            else {
+                btnEstimate.disabled = false;
+            }
+        });
+
+        function estimateTotal(event){
+            event.preventDefault();
+            alert("test");
+        }
+
+    });
 
 })();
