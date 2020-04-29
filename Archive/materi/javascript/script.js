e97c952c59e6f6b5c751bcb3d525c923d3bb18dc
Ieffe <novditha@gmail.com>
2020-04-29 15:32:02 +0800

    video part 3

--- Archive/materi/javascript/script.js
+++ Archive/materi/javascript/script.js
@@ -12,7 +12,8 @@
     document.addEventListener("DOMContentLoaded", function(){
         var state = document.getElementById("s-state");
 
-        document.getElementById("cart-hplus").addEventListener("submit", estimateTotal);
+        document.getElementById("cart-hplus")
+        .addEventListener("submit", estimateTotal);
 
         var btnEstimate = document.getElementById("btn-estimate");
 
@@ -29,9 +30,49 @@
 
         function estimateTotal(event){
             event.preventDefault();
-            alert("test");
-        }
+            if (state.value === "") {
+               alert("Silakan Pilih Negara Pengiriman!"); 
+              
+               state.focus();
+               return;
+            } 
+        
 
+            var itemBall = document.getElementById("txt-q-bball").value,
+            itemJersey = document.getElementById("txt-q-jersey").value,
+            itemPower = document.getElementById("txt-q-power").value,
+            shippingState = state.value,
+            shippingMethod = document.querySelector("[name=r_method]:checked").value;
+
+            var totalQty = itemBall + itemJersey + itemPower,
+            shippingCostPer, shippingCost, taxFactor = 1, estimate, totalItemPrice = (90 * itemBall) + (25 * itemJersey) + (30 * itemPower);
+
+            if (shippingState === "CA"){
+                taxFactor = 1.075;
+            }
+
+            switch (shippingMethod) {
+                case "usps":
+                    shippingCostPer = 2;
+                    break;
+
+                case "ups":
+                    shippingCostPer = 3;
+                    break;
+                
+                default: 
+                    shippingCostPer = 0;
+                    break; 
+            }
+
+            shippingCost = shippingCostPer * totalQty;
+
+            estimate = (totalItemPrice * taxFactor) + shippingCost;
+            
+            document.getElementById("txt-estimate").value = estimate; 
+            
+        }
+    
     });
 
 })();
